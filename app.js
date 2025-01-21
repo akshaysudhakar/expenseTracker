fa0f32e0b1be4e1d33844fa4875c8faff492e231
akshaysudhakar <akshaysudhakarsmg@gmail.com>
2025-01-21 19:54:53 +0530

    //changed success url post payment. commented out unnecessary apis for localhost

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 const http = require("http");
 require('dotenv').config(); // Load environment variables from .env file
 
-const cors = require('cors');
-const helmet = require('helmet');
-const compression = require('compression');
-const morgan = require('morgan');
+// const cors = require('cors');
+// const helmet = require('helmet');
+// const compression = require('compression');
+// const morgan = require('morgan');
 
 const sequelize = require('./util/database');
 const user = require('./models/user');
@@ -20,32 +20,29 @@ const userroute = require('./routes/userroute');
 const premiumroute = require('./routes/premium');
 const passwordroute = require('./routes/password');
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+// const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 
 const app = express();
 
 // Load your private key and certificate
-const privateKey = fs.readFileSync('server.key');
-const certificate = fs.readFileSync('server.cert');
+// const privateKey = fs.readFileSync('server.key');
+// const certificate = fs.readFileSync('server.cert');
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-    origin: true, // Allow requests from your Nginx server
-    methods: 'GET,POST,PUT,DELETE',
-    credentials: true, // Include credentials if necessary
-}));
-app.use(helmet());
-app.use(compression());
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cors({
+//     origin: true, // Allow requests from your Nginx server
+//     methods: 'GET,POST,PUT,DELETE',
+//     credentials: true, // Include credentials if necessary
+// }));
+// app.use(helmet());
+// app.use(compression());
 //app.use(morgan('combined', { stream: accessLogStream }));
 
 // Set up your routes
-app.use('/admin', 
-    (req, res, next) => {
-        console.log('A request in the admin path');
-        next();
-    }, 
-    adminroute
-);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminroute);
 app.use('/user', userroute);
 app.use('/premium', premiumroute);
 app.use('/password', passwordroute);
@@ -66,7 +63,7 @@ sequelize.sync()
         const PORT = process.env.PORT || 3000;
         
         // Create an HTTPS server
-        const httpsServer = http.createServer({ key: privateKey, cert: certificate }, app);
+        const httpsServer = http.createServer(app);
         
         // Listen on the specified port
         httpsServer.listen(PORT, () => console.log(`Server is ready on port ${PORT}`));
--- routes/admin.js
+++ routes/admin.js
@@ -6,29 +6,16 @@ const authenticate = require('./../middleware/auth');
 const admincantroller = require('./../cantroller/admincantrole');
 
 // Middleware to log the request body before parsing with express.json
-function logBeforeJson(req, res, next) {
-    console.log('Request body before express.json:');
-    next();
-}
-
-// Middleware to log the request body after parsing with express.urlencoded
-function logAfterUrlencoded(req, res, next) {
-    console.log('Request body after express.urlencoded:');
-    next();
-}
 
 route.post(
-    '/signup',
-    logBeforeJson,                    // Logs body before express.json()
-    express.json(),
-    logAfterUrlencoded,               // Logs body after express.urlencoded()
+    '/signup',                   // Logs body before express.json()
+    express.json(),             // Logs body after express.urlencoded()
     express.urlencoded({ extended: true }),
     admincantroller.addUser
 );
-
-route.post('/buy_premium', logBeforeJson,  authenticate, admincantroller.premium_handler);
+route.post('/buy_premium', authenticate, admincantroller.premium_handler);
 route.post('/buy_premium_status', bodyParser.raw({ type: 'application/json' }), admincantroller.paymentHandler);
-route.get('/confirm_payment', logBeforeJson ,authenticate, admincantroller.confirmPayment);
+route.get('/confirm_payment',authenticate, admincantroller.confirmPayment);
 
 module.exports = route;
 
